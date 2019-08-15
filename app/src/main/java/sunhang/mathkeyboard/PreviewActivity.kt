457eb0db60d1b6635ef5ac86ad50c903ac426498
sunhang <sunhang09@gmail.com>
2019-08-15 11:16:38 +0800

    添加调试键盘的代码

--- app/src/main/java/sunhang/mathkeyboard/PreviewActivity.kt
+++ app/src/main/java/sunhang/mathkeyboard/PreviewActivity.kt
@@ -1,15 +1,20 @@
 package sunhang.mathkeyboard
 
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
+import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_preview.*
 import sunhang.mathkeyboard.ime.IMSContext
 import sunhang.mathkeyboard.ime.InputToEditor
 import sunhang.mathkeyboard.ime.kbdcontroller.RootController
 import sunhang.mathkeyboard.kbdviews.RootView
+import sunhang.openlibrary.uiLazy
 
 class PreviewActivity : AppCompatActivity(){
 
@@ -21,8 +26,18 @@ class PreviewActivity : AppCompatActivity(){
         fl.addView(rootView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
 
         val inputToEditor = InputToEditor()
+        inputToEditor.currentInputConnection = (et as EditTextForKbdDebug).inputConnection
+
         val rootController = RootController()
         rootController.onCreate(IMSContext(this, inputToEditor), rootView)
         rootController.onCreateInputViewInvoked()
+    }
+
+    class EditTextForKbdDebug(context: Context?, attrs: AttributeSet?) : EditText(context, attrs) {
+        val inputConnection by uiLazy {
+            val editorInfo = EditorInfo()
+            editorInfo.packageName = this.context.packageName
+            onCreateInputConnection(editorInfo)
+        }
     }
 }
