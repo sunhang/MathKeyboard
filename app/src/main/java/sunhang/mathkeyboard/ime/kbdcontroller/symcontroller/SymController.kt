699c5f1a2570683275b0ee7181344d7a2495207e
sunhang <sunhang09@gmail.com>
2019-10-02 19:57:24 +0800

    添加tablayout

--- app/src/main/java/sunhang/mathkeyboard/ime/kbdcontroller/symcontroller/SymController.kt
+++ app/src/main/java/sunhang/mathkeyboard/ime/kbdcontroller/symcontroller/SymController.kt
@@ -2,6 +2,9 @@ package sunhang.mathkeyboard.ime.kbdcontroller.symcontroller
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.kbd_sym.view.*
 import sunhang.mathkeyboard.R
 import sunhang.mathkeyboard.ime.IMSAction
 import sunhang.mathkeyboard.ime.IMSContext
@@ -10,15 +13,50 @@ import sunhang.mathkeyboard.kbdskin.SkinAttrUser
 import sunhang.mathkeyboard.kbdviews.RootView
 import sunhang.openlibrary.uiLazy
 
+
 class SymController(private val imsContext: IMSContext, rootView: RootView) : BaseController(),
     IMSAction, SkinAttrUser {
     private val containter = rootView.findViewById<ViewGroup>(R.id.ime_layer)
     private val context = imsContext.context
-    private val layout by uiLazy { View.inflate(context, R.layout.kbd_sym, null) }
+    private val layout by uiLazy {
+        View.inflate(context, R.layout.kbd_sym, null).apply {
+            with(viewpager) {
+                addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tablayout))
+                adapter = Adapter()
+            }
+
+            with(tablayout) {
+                context.resources.getStringArray(R.array.symbol_titles).forEach { title ->
+                    addTab(newTab().apply {
+                        text = title
+                    })
+                }
+            }
+
+            tablayout.addOnTabSelectedListener(object :
+                TabLayout.BaseOnTabSelectedListener<TabLayout.Tab> {
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    val position = tab?.position ?: 0
+                    viewpager.currentItem = position
+                }
+
+            })
+
+        }
+    }
+
 
     fun show() {
         if (layout.parent == null) {
-            containter.addView(layout, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            containter.addView(
+                layout,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                imsContext.imeLayoutConfig.toolbarHeight
+            )
         }
     }
 
@@ -26,5 +64,16 @@ class SymController(private val imsContext: IMSContext, rootView: RootView) : Ba
         if (layout.parent != null) {
             containter.removeView(layout)
         }
+    }
+
+    class Adapter : PagerAdapter() {
+        override fun isViewFromObject(view: View, `object`: Any): Boolean {
+            return true
+        }
+
+        override fun getCount(): Int {
+            return 0
+        }
+
     }
 }
