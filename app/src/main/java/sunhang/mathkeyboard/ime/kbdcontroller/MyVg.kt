10cd7e897652702a6140635467e9bd91810bba29
sunhang <sunhang09@gmail.com>
2020-06-20 23:55:13 +0800

    解决布局问题

--- app/src/main/java/sunhang/mathkeyboard/ime/kbdcontroller/MyVg.kt
+++ app/src/main/java/sunhang/mathkeyboard/ime/kbdcontroller/MyVg.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import android.os.Handler
 import android.util.AttributeSet
 import android.view.ViewGroup
+import android.widget.FrameLayout
 
-class MyVg(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
+class MyVg(context: Context?, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
-    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        val child = getChildAt(0)
-        child.layout(0, t, r, b)
-    }
+//    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+//        val child = getChildAt(0)
+//        child.layout(0, t, r, b)
+//    }
 }
